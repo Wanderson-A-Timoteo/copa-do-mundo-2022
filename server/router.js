86590a9a01db08746e387f89cc3037b7f74bce9b
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-10-18 22:13:09 -0400

    Add função para listar todos os jogos e add biblioteca date-fns para filtrar os jogos pela data

--- server/router.js
+++ server/router.js
@@ -2,11 +2,14 @@ import Router from "@koa/router"
 
 import * as users from "./app/users/index.js"
 import * as hunches from "./app/hunches/index.js"
+import * as games from "./app/games/index.js"
 
 export const router = new Router();
 
 router.get('/users', users.list)
 router.post('/users', users.create)
 
-//router.post('/hunches', hunches.list)
+//router.get('/hunches', hunches.list)
 router.post('/hunches', hunches.create)
+
+router.get('/games', games.list)
