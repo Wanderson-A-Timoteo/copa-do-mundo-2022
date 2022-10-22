edf78c122a26370c65cc3678e2d4a4d031034ceb
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2022-10-22 12:02:59 -0400

    Add integração web e servidor, função p criar user com axios, formik e yup para validação do form.

--- web/src/pages/Signup/index.jsx
+++ web/src/pages/Signup/index.jsx
@@ -1,6 +1,38 @@
+import axios from 'axios'
+import { useFormik } from 'formik'
+import * as yup from 'yup'
+
 import { Icon, Input } from '~/components'
 
+const validationSchema = yup.object().shape({
+    name: yup.string().required('Preencha seu nome'),
+    username: yup.string().required('Preencha seu nome de usuário'),
+    email: yup.string().email('Informe um email válido').required('Informe seu email'),
+    password: yup.string().required('Digite sua senha')
+})
+
 export const Signup = () => {
+
+    const formik = useFormik({
+        onSubmit: async (values) => {
+            const res = await axios({
+                method: 'post',
+                baseURL: 'http://localhost:3000',
+                url: '/users',
+                data: values
+            })
+
+            console.log(res.data)
+        },
+        initialValues: {
+            name: '',
+            username: '',
+            email: '',
+            password: ''
+        },
+        validationSchema
+    })
+
     return (
         <div className="">
             <header className="p-4 border-b border-red-300">
@@ -17,34 +49,54 @@ export const Signup = () => {
                     <h2 className="text-xl font-bold">Crie sua conta</h2>
                 </div>
 
-                <form className="p-4 space-y-6">
+                <form className="p-4 space-y-6" onSubmit={formik.handleSubmit}>
                     <Input
                         type="text"
                         name="name"
                         label="Seu nome"
                         placeholder="Digite seu nome"
+                        error={formik.touched.name && formik.errors.name}
+                        value={formik.values.name}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />    
                     <Input
                         type="text"
                         name="username"
                         label="Seu nome de usuário"
-                        placeholder="Digite sua senha"
+                        placeholder="Digite seu nome de usuário"
+                        error={formik.touched.username && formik.errors.username}
+                        value={formik.values.username}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
                     <Input
                         type="text"
                         name="email"
                         label="Seu e-mail"
                         placeholder="Digite seu e-mail"
+                        error={formik.touched.email && formik.errors.email}
+                        value={formik.values.email}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />    
                     <Input
                         type="password"
                         name="password"
                         label="Sua senha"
                         placeholder="Digite sua senha"
+                        error={formik.touched.password && formik.errors.password}
+                        value={formik.values.password}
+                        onChange={formik.handleChange}
+                        onBlur={formik.handleBlur}
                     />
-                     <a href='/dashboard' className="w-full text-center text-white bg-red-500 px-6 py-3 rounded-xl">
+                     <button 
+                        className="block w-full text-center text-white bg-red-500 px-6 py-3 rounded-xl disabled:opacity-50" 
+                        type='submit' 
+                        disabled={!formik.isValid}
+                    >
                         Criar minha conta
-                    </a>
+                    </button>
                 </form>
             </main>
         </div>
